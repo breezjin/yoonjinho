2e06a3c3693df89ca351b92f9253aaa346776152
breezjin <breezjin@gmail.com>
2023-04-03 12:40:41 +0900

    refactor: post page

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,11 +1,8 @@
 import 'github-markdown-css';
 
-import Markdown from 'markdown-to-jsx';
 import { NotionToMarkdown } from 'notion-to-md';
 import ReactMarkdown from 'react-markdown';
 
-import clientGetPageProperties from '@/app/api/notion/clientGetPageProperties';
-import PostContent from '@/components/PostsContent';
 import notion from '@/sdk/notion';
 
 const n2m = new NotionToMarkdown({ notionClient: notion });
@@ -21,12 +18,10 @@ export default async function Post({
   const mdString = n2m.toMarkdownString(mdblocks);
   // const postProperties = await clientGetPageProperties(params.id);
   // console.log('post >>>', postProperties);
-  console.log('mdblocks >>>', mdblocks);
   console.log('mdString >>>', mdString);
 
   return (
     <>
-      {/* <Markdown>{mdString}</Markdown> */}
       <ReactMarkdown>{mdString}</ReactMarkdown>
     </>
   );
