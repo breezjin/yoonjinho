ec7147af03909a3819a3ce7a465351de0379decd
breezjin <breezjin@gmail.com>
2023-04-07 12:03:09 +0900

    feat: 스타일 정리 및 헤더메뉴 정리

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,21 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
 
+const menus = [
+  { href: '/', label: 'Home' },
+  { href: '/blog', label: 'Blog' },
+  { href: '/letter', label: 'Letter' },
+  { href: '/about', label: 'About' },
+];
+
 export default function Header() {
   return (
     <nav className={clsx('b-8 flex h-24 w-screen flex-col')}>
+      {menus.map(menu => (
+        <Link key={menu.href} href={menu.href}>
+          {menu.label}
+        </Link>
+      ))}
       <Link href={'/'}>Home</Link>
     </nav>
   );
